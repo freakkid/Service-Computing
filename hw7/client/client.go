15f0b5a2270175b9d6a6db0043f756f98c477971
freakkid <904811062@qq.com>
2017-12-27 21:04:21 +0800

    modify

--- hw7/client/client.go
+++ hw7/client/client.go
@@ -1,30 +1,29 @@
 package client
 
 import (
-	//"strings"
-	"encoding/json"
 	"crypto/md5"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
-	"time"
 	"strconv"
+	"time"
 )
 
 const (
-	min = 100000
-	max = 999999
-	from = "en"
-	to = "zh"
+	min   = 100000
+	max   = 999999
+	from  = "en"
+	to    = "zh"
 	appid = "20171226000109327"
-	key = "pkMKVbG3W56acsG8JPJ7"
+	key   = "pkMKVbG3W56acsG8JPJ7"
 )
 
 type resultJSON struct {
-	From string `json:"from"`
-	To string `json:"to"`
+	From        string              `json:"from"`
+	To          string              `json:"to"`
 	TransResult []map[string]string `json:"trans_result"`
 }
 
@@ -47,8 +46,8 @@ func HTTPGet(q string) string {
 	return q + ": Ocurr error when parsing json response"
 }
 
-// HTTPGetWithChannel .
-func HTTPGetWithChannel(q string, s chan string) {
+// HTTPGetAsync .
+func HTTPGetAsync(q string, s chan string) {
 	resp, err := http.Get(generateURL(q))
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -61,7 +60,7 @@ func HTTPGetWithChannel(q string, s chan string) {
 	}
 	var resultjson resultJSON
 	if err = json.Unmarshal(body, &resultjson); err == nil {
-		s <-fmt.Sprintln(resultjson.TransResult[0]["src"], resultjson.TransResult[0]["dst"]) 
+		s <- fmt.Sprintln(resultjson.TransResult[0]["src"], resultjson.TransResult[0]["dst"])
 	} else {
 		fmt.Println(q + ": Ocurr error when parsing json response")
 		s <- ""
@@ -72,12 +71,12 @@ func HTTPGetWithChannel(q string, s chan string) {
 func generateURL(q string) string {
 	salt := generateSalt()
 	sign := generateSign(q, salt)
-	return "http://api.fanyi.baidu.com/api/trans/vip/translate?q="+q+"&from="+from+"&to="+to+"&appid=20171226000109327&salt="+salt+"&sign="+sign
+	return "http://api.fanyi.baidu.com/api/trans/vip/translate?q=" + q + "&from=" + from + "&to=" + to + "&appid=20171226000109327&salt=" + salt + "&sign=" + sign
 }
 
 // appid + q + salt + key
 func generateSign(q string, salt string) string {
-	return encryptByMD5("20171226000109327"+ q + salt + key)
+	return encryptByMD5("20171226000109327" + q + salt + key)
 }
 
 func generateSalt() string {
@@ -88,6 +87,7 @@ func getRandInt(min, max int) string {
 	rand.Seed(time.Now().UTC().UnixNano())
 	return strconv.Itoa(min + rand.Intn(max-min))
 }
+
 func encryptByMD5(data string) string {
 	return fmt.Sprintf("%x", md5.Sum([]byte(data)))
 }
--- hw7/main.go
+++ hw7/main.go
@@ -1,39 +1,46 @@
 package main
 
 import (
-	"time"
 	"fmt"
 	"github.com/freakkid/Service-Computing/hw7/client"
+	"time"
 )
 
-func test1() {
+func syncTest() {
 	start := time.Now()
+	
 	x := client.HTTPGet("kid")
 	y := client.HTTPGet("sherry")
 	z := client.HTTPGet("bingo")
 	m := client.HTTPGet("lock")
 	n := client.HTTPGet("exit")
-	fmt.Printf("%s", x + y + z + m + n)
+
+	fmt.Printf("%s", x+y+z+m+n)
 	fmt.Println("Time:", time.Since(start))
 }
 
-func test2() {
+func asyncTest() {
 	s := make(chan string)
 	start := time.Now()
-	go client.HTTPGetWithChannel("kid", s)
-	go client.HTTPGetWithChannel("sherry", s)
-	go client.HTTPGetWithChannel("bingo", s)
-	go client.HTTPGetWithChannel("lock", s)
-	go client.HTTPGetWithChannel("exit", s)
-	fmt.Printf("%s", <-s + <-s + <-s + <-s + <-s)
+
+	go client.HTTPGetAsync("kid", s)
+	go client.HTTPGetAsync("sherry", s)
+	go client.HTTPGetAsync("bingo", s)
+	go client.HTTPGetAsync("lock", s)
+	go client.HTTPGetAsync("exit", s)
+
+	fmt.Printf("%s", <-s+<-s+<-s+<-s+<-s)
 	fmt.Println("Time:", time.Since(start))
 }
 
 func main() {
 	fmt.Printf("%s\n\n", "使用 go HTTPClient 实现图 6-2 的 Naive Approach")
-	test1()
+	syncTest()
+
 	fmt.Println("------------------------------------")
+
 	fmt.Printf("%s\n\n", "为每个 HTTP 请求设计一个 goroutine ，利用 Channel 搭建基于消息的异步机制，实现图 6-3")
-	test2()
+	asyncTest()
+
 	fmt.Println("\n[本次作业使用了百度翻译API@百度翻译]")
 }
